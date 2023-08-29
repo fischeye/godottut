7a80ed247a62a4ceab58afc359227fd0bd259775
j.fischer <j.fischer@huettinger.de>
2023-08-29 13:18:08 +0200

    Enemy Adjustments

--- things/enemy.cs
+++ things/enemy.cs
@@ -9,9 +9,13 @@ public partial class enemy : CharacterBody2D
     private AnimatedSprite2D enemySprites;
     private bool foundPlayer;
 
+    private bool isDied;
+    private long tickDead;
+
     public override void _Ready()
     {
         enemySprites = GetNode<AnimatedSprite2D>("enemySprites");
+        isDied = false;
     }
 
     public void _on_player_detection_body_entered(Node2D body)
@@ -30,17 +34,36 @@ public partial class enemy : CharacterBody2D
         }
     }
 
+    public void _on_player_death_body_entered(Node2D body)
+    {
+        if (body.Name == "player") {
+            isDied = true;
+            //enemySprites.Play("death");
+            tickDead = DateTime.UtcNow.Ticks + (TimeSpan.TicksPerMillisecond * 500);
+            GD.Print("Enemy is dead!");
+        }
+    }
     
     public override void _PhysicsProcess(double delta)
     {
         Vector2 velocity = Velocity;
         velocity.Y += gravity * (float)delta;
 
-        if (foundPlayer)
+        if (foundPlayer && !isDied)
         {
             enemySprites.Play("attack");
         } else {
             enemySprites.Play("idle");
+        }
+
+        if (isDied)
+        {
+            enemySprites.Play("death");
+            long curTick = DateTime.UtcNow.Ticks;
+            if (curTick > tickDead)
+            {
+                QueueFree();
+            }
         }
 
         Velocity = velocity;
